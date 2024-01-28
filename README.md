2fb2d0a85a46cb349156b2822319ca57b4d5bd3b
Peter Bignold <noodlegamesdev@gmail.com>
2024-01-28 14:19:13 -0700

    Adjusted Routing to work with Github Pages
    - Changed to HashRouter

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Portfolio Website - Peter Bignold
 ### Live Site: https://peter-bignold.github.io/portfolio/
-A portfolio website that serves as a collection of my personal projects. Created in plain html / css / js.
+A portfolio website that serves as a collection of my personal projects. Created using React.
+
+## Build ##
+npm run deploy -- -m "Commit Message"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import ScrollToTop from "./components/ScrollToTop.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
+    <HashRouter>
         <ScrollToTop/>
         <App/>
-    </Router>
+    </HashRouter>
 );
