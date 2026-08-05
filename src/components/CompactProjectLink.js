a6ede9c88d68476e69acd6268d014175b6888f02
Peter Bignold <noodlegamesdev@gmail.com>
2026-08-04 21:11:17 -0600

    Backdrop blurs

--- src/components/CompactProjectLink.js
+++ src/components/CompactProjectLink.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const CompactProjectLink = ({to, logo, title, meta}) => (
   <Link
-    className="m-2 flex w-full flex-col rounded border border-slate-800 bg-slate-800/50 px-6 py-5 text-left text-neutral-100 shadow-panel transition duration-200 hover:bg-slate-800 md:w-auto md:min-w-72"
+    className="m-2 flex w-full flex-col rounded border border-slate-800 bg-slate-800/50 backdrop-blur px-6 py-5 text-left text-neutral-100 shadow-panel transition duration-200 hover:bg-slate-800 md:w-auto md:min-w-72"
     to={to}
   >
     <div className="flex items-start">
--- src/components/SkillChip.js
+++ src/components/SkillChip.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SkillChip = ({children}) => (
-  <div className="m-1 flex max-h-10 rounded-2xl border border-slate-800 bg-slate-800/50 px-4 py-1.5 text-center">
+  <div className="m-1 flex max-h-10 rounded-2xl border border-slate-800 bg-slate-800/50 backdrop-blur px-4 py-1.5 text-center">
     <p className="m-0 mt-1.5 text-base font-light text-neutral-200 md:text-xl">{children}</p>
   </div>
 );
