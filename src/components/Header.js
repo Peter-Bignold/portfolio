e8c62ad286c4a5ee06bb429aff595781418b4b0f
Peter Bignold <noodlegamesdev@gmail.com>
2026-08-04 22:09:23 -0600

    Header and footer refinement

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,39 @@ import {Link, useLocation} from 'react-router-dom';
 
 const Header = () => {
   const url = useLocation().pathname;
-  const getLinkClassName = (isActive) => [
-    'mx-2.5 my-1 mt-1 block bg-none px-0 py-1.5 text-center text-xl font-normal text-neutral-400 transition duration-200 hover:text-neutral-100 md:mx-0 md:w-24',
-    isActive ? 'text-neutral-100 underline decoration-[1.2px] underline-offset-[20%]' : '',
-  ].join(' ');
+  const getLinkClass = (isActive, isHome = false) => {
+    if (isHome) {
+      return [
+        'ml-3 mt-1.5 text-xl font-light text-neutral-400 hover:text-neutral-100 whitespace-nowrap transition',
+        isActive ? '!text-neutral-100' : '',
+      ].join(' ')
+    }
+    return [
+      'mx-4 mt-1 block text-xl font-light text-neutral-400 transition duration-200 hover:text-neutral-100',
+      isActive ? '!text-neutral-100 underline decoration-[1px] underline-offset-[30%]' : '',
+    ].join(' ')
+  };
   
   return (
     <div className="fixed top-0 z-[1000] m-0 w-full">
       <header className="flex h-14 items-center justify-center border-b border-slate-800/80 bg-slate-900/50 px-[6%] py-2.5 text-3xl text-neutral-100 shadow-soft backdrop-blur md:justify-between">
-        <div className="hidden flex-row items-center text-left md:flex bg-slate-800 py-1 px-4 rounded-lg">
+        <Link to="/" className="hidden flex-row items-center text-left md:flex rounded-lg">
           <img alt="Peter Bignold" src={require("assets/images/home/headshot3.jpg")} className="h-7 w-7 rounded-full"></img>
-          <h3 className="ml-3 mt-1.5 text-xl font-light whitespace-nowrap">Peter Bignold</h3>
-        </div>
+          <h3 className={getLinkClass(url === '/', true)}>Peter Bignold</h3>
+        </Link>
         <div className="mt-1 flex flex-row justify-end text-right">
           <Link 
-            className={getLinkClassName(url.includes('games'))} 
+            className={getLinkClass(url.includes('games'))} 
             to="/games">
             Software
           </Link>
           <Link 
-            className={getLinkClassName(url.includes('artwork'))} 
+            className={getLinkClass(url.includes('artwork'))} 
             to="/artwork">
             Artwork
           </Link>
           <Link 
-            className={getLinkClassName(url.includes('contact'))} 
+            className={getLinkClass(url.includes('contact'))} 
             to="/contact">
             Contact
           </Link>
